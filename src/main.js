2fb8c2fd9c5c57f71fe75e1cc4a32da38c685c90
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-09-20 22:16:44 +0200

    Delete additional Checking the same Search request

--- src/main.js
+++ src/main.js
@@ -38,30 +38,6 @@ searchForm.addEventListener("submit", (event) => {
 		});
 	}
 
-	// If this is the same Search request
-	if (inputSearchValue === searchRequest) {
-		return iziToast.warning({
-			class: "caution",
-			titleColor: "#fff",
-			titleSize: "16px",
-			titleLineHeight: "1.5",
-			message: "Results for this Search query are already shown",
-			backgroundColor: "#ffa000",
-			color: "white",
-			messageColor: "#fff",
-			messageSize: "16px",
-			messageLineHeight: "1.5",
-			iconUrl: new URL("./img/alert.svg", import.meta.url).href,
-			iconColor: "#fff",
-			close: true,
-			closeOnEscape: true,
-			progressBarColor: "#ffe0ac",
-			position: "topRight",
-			timeout: 3000,
-			animateInside: false,
-			transitionIn: "bounceInLeft"
-		});
-	}
 	searchRequest = inputSearchValue;
 
 	// Get images
