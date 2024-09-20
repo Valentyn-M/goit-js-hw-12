b82992be1870d7668dbd148066699dd212c4ebd7
Valentyn Mostovyi <mostovoy.v@gmail.com>
2024-09-20 22:11:41 +0200

    Fix Checking the Search request

--- src/main.js
+++ src/main.js
@@ -13,34 +13,8 @@ searchForm.addEventListener("submit", (event) => {
 	const inputSearch = searchForm.elements.search;
 	const inputSearchValue = inputSearch.value.trim();
 
-	// If this is the same Search request
-	if (inputSearchValue === searchRequest) {
-		return iziToast.warning({
-			class: "caution",
-			titleColor: "#fff",
-			titleSize: "16px",
-			titleLineHeight: "1.5",
-			message: "Results for this Search query are already shown",
-			backgroundColor: "#ffa000",
-			color: "white",
-			messageColor: "#fff",
-			messageSize: "16px",
-			messageLineHeight: "1.5",
-			iconUrl: new URL("./img/alert.svg", import.meta.url).href,
-			iconColor: "#fff",
-			close: true,
-			closeOnEscape: true,
-			progressBarColor: "#ffe0ac",
-			position: "topLeft",
-			timeout: 2500,
-			animateInside: false,
-			transitionIn: "bounceInRight"
-		});
-	}
-	searchRequest = inputSearchValue;
-
 	// If the Search request is empty
-	if (searchRequest === "") {
+	if (inputSearchValue === "") {
 		return iziToast.warning({
 			class: "caution",
 			titleColor: "#fff",
@@ -63,6 +37,32 @@ searchForm.addEventListener("submit", (event) => {
 			transitionIn: "bounceInLeft"
 		});
 	}
+
+	// If this is the same Search request
+	if (inputSearchValue === searchRequest) {
+		return iziToast.warning({
+			class: "caution",
+			titleColor: "#fff",
+			titleSize: "16px",
+			titleLineHeight: "1.5",
+			message: "Results for this Search query are already shown",
+			backgroundColor: "#ffa000",
+			color: "white",
+			messageColor: "#fff",
+			messageSize: "16px",
+			messageLineHeight: "1.5",
+			iconUrl: new URL("./img/alert.svg", import.meta.url).href,
+			iconColor: "#fff",
+			close: true,
+			closeOnEscape: true,
+			progressBarColor: "#ffe0ac",
+			position: "topRight",
+			timeout: 3000,
+			animateInside: false,
+			transitionIn: "bounceInLeft"
+		});
+	}
+	searchRequest = inputSearchValue;
 
 	// Get images
 	getImagesPixabay(searchRequest, true).then((images) => {
